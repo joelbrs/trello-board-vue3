db0b7f27048dbb35ad304e2f73fb5b6c3e5ae3ea
Joel Filho <joelfilho02812@gmail.com>
2023-12-01 12:06:00 -0300

    refactor: adding trello logo

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+  <div class="p-10 h-[100vh] bg-red-600 overflow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10">
+      <img
+        class="mr-3"
+        width="200"
+        src="assets/trello_logo.png"
+        alt="Trello Logo"
+      />
+      Board
+    </h1>
     <TrelloBoard />
   </div>
 </template>
